d597d6234560a2d2acc388b25b9c36fdf1403edb
nghiaduongtan <40726387+nghiaduongtan@users.noreply.github.com>
2018-07-04 23:45:55 +0700

    Update and rename generate final submission.py to generate_final_submission.py

--- generate_final_submission.py
+++ generate_final_submission.py
@@ -14,19 +14,20 @@ from sklearn.preprocessing import MinMaxScaler
 from multiprocessing.dummy import Pool as ThreadPool
 pool = ThreadPool(16)
 
-with open('Y_training_pid_trackid_new_rating_csr.pkl','rb') as f:
+# Necessary files can be downloaded from OneDrive (see file 'db'). Put these files under the same directory with .py files. 
+with open('Y_training_pid_trackid_new_rating_csr.pkl', 'rb') as f:
     Y_training_pid_track_id_rating_sparse_csr = pickle.load(f)
 
-with open('Y_challenge_1_5_10_25_100track_pidnew_trackid_rating_csr.pkl','rb') as f:
+with open('Y_challenge_1_5_10_25_100track_pidnew_trackid_rating_csr.pkl', 'rb') as f:
     Y_challenge_track_pidnew_rating_sparse_csr = pickle.load(f)
 
 Y_training_pid_track_id_rating_sparse_csr = Y_training_pid_track_id_rating_sparse_csr.T
-Y_training = sparse.vstack([Y_challenge_track_pidnew_rating_sparse_csr, Y_training_pid_track_id_rating_sparse_csr],"csr")
+Y_training = sparse.vstack([Y_challenge_track_pidnew_rating_sparse_csr, Y_training_pid_track_id_rating_sparse_csr], 'csr')
 
 W, X = implicit.alternating_least_squares((Y_training*50).astype('double'), 
-                                                          factors = 400, 
-                                                          regularization = 0.01, 
-                                                          iterations = 50, use_gpu=False)
+                                                          factors=400, 
+                                                          regularization=0.01, 
+                                                          iterations=50, use_gpu=False)
 
 
 def rec_items(pid):
@@ -35,7 +36,7 @@ def rec_items(pid):
     pref_vec[pref_vec > 1] = 0
     rec_vector = W[pid].dot(X.T)
     min_max = MinMaxScaler()
-    rec_vector_scaled = min_max.fit_transform(rec_vector.reshape(-1,1))[:,0]
+    rec_vector_scaled = min_max.fit_transform(rec_vector.reshape(-1, 1))[:, 0]
     recommend_vector = pref_vec*rec_vector_scaled
     idx = np.argsort(recommend_vector)[::-1][:500]
     recommendation = pd.DataFrame(idx)
@@ -49,15 +50,15 @@ trackid_trackuri = pd.read_csv('trackid_trackuri_counts.csv')
 id_uri_dict = trackid_trackuri.track_uri.to_dict()
 recommend_list_final_ax = recommend_list_final.applymap(lambda x: id_uri_dict.get(x) if x in id_uri_dict else x)
 
-with open('data_new_rating_5th/Y_challenge_1_5_10_25_100track_pidnew_pid_trackid_rating.pkl','rb') as f:
+with open('Y_challenge_1_5_10_25_100track_pidnew_pid_trackid_rating.pkl', 'rb') as f:
     Y_challenge_1_5_10_25_100track_pidnew_pid_trackid_rating = pickle.load(f)
 
 pid = Y_challenge_1_5_10_25_100track_pidnew_pid_trackid_rating.pid.drop_duplicates(keep='first')
 pid.index = range(pid.shape[0])
 
 recommend_list_final_ax.index = range(recommend_list_final_ax.shape[0])
-submission_1_5_10_25_100track = pd.concat([pid,recommend_list_final_ax],axis=1,ignore_index=True)
+submission_1_5_10_25_100track = pd.concat([pid, recommend_list_final_ax], axis=1, ignore_index=True)
 
-submission_recommend_for_pid_0_track_using_title = pd.read_csv('data_new_rating_5th/recommend_pid_0track_using_title.csv',header=None)
-submission_recommend_for_challenge_set = pd.concat([submission_1_5_10_25_100track,submission_recommend_for_pid_0_track_using_title],axis=0,ignore_index=True)
-submission_recommend_for_challenge_set.to_csv('data_new_rating_5th/github_submission_recommend_for_challenge_set.csv',index=False,header=False)
+submission_recommend_for_pid_0_track_using_title = pd.read_csv('recommend_pid_0track_using_title.csv', header=None)
+submission_recommend_for_challenge_set = pd.concat([submission_1_5_10_25_100track, submission_recommend_for_pid_0_track_using_title], axis=0, ignore_index=True)
+submission_recommend_for_challenge_set.to_csv('submission_for_challenge_set.csv', index=False, header=False)
